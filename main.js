a069c94c98bcee539a3777f8991c31a50761d1b2
hamo123-vi <amil.valjevac@stu.ibu.edu.ba>
2021-07-17 16:58:20 +0200

    Frontend preliminary completed

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
+new Vue({
+    el: '#messages',
+    data: 
+    {
+        items: [
+            { message: 'Foo' },
+            { message: 'Bar' },
+            { message: 'Foo' },
+            { message: 'Foo' },
+            { message: 'Foo' },
+            { message: 'Foo' },
+            { message: 'Ti si bila vec zena udatadj'+
+                           'gčds  jčglk jsdlčkjglksfdjl'+
+                        'ddgf ffffff fflkfff fffa' },
+            { message: 'Bar' },
+            { message: 'Bar' },
+            { message: 'lfkdjfaslkjfdskaljflkjdsaldjdfaslkjfalksjfakddfsfsdfsdfdfsfddddddddsdfsdsddddddddddd' },
+            { message: 'Bar' },
+            { message: 'Foo' }
+        ]
+    }
+  })
+
+  new Vue({
+    el: '#users',
+    data: 
+    {
+
+        users: [
+            { username : 'user001'},
+            { username : 'user003'},
+            { username : 'user005'},
+            {username : 'user009'}
+        ]
+    }
+  })
+
 function sidebarToggler()
 {
     if(document.getElementById("sidebar").style.display === "block")
