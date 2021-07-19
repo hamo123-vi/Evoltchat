517349d3fae28d2d2deb1c21975b86738cd5fc78
hamo123-vi <amil.valjevac@stu.ibu.edu.ba>
2021-07-19 12:12:22 +0200

    API organization bone, install Flight framework via Composer and setting up Config class

---
+++ api/Config.Class.php
@@ -0,0 +1,9 @@
+<?php 
+
+    class Config
+    {
+        const DB_HOST="localhost";
+        const DB_USERNAME="evolt";
+        const DB_PASSWORD="evolt197";
+        const DB_SCHEME="evolt";
+    }
