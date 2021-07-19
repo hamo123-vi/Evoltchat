0254ce6d060f18a2605119af0e6907bf197e3956
hamo123-vi <amil.valjevac@stu.ibu.edu.ba>
2021-07-19 16:28:12 +0200

    Implemented loadMessages() function in Messages BL layer

---
+++ api/services/MessagesService.Class.php
@@ -0,0 +1,19 @@
+<?php 
+    require_once dirname(__FILE__)."/../dao/MessagesDao.Class.php";
+    #use \Firebase\JWT\JWT;
+    class MessagesService 
+    {
+        
+        private $dao;
+        public function __construct()
+        {
+            $this->dao=new MessagesDao();
+        }
+
+        public function loadMessages()
+        {
+            return $this->dao->getMessages();
+        }
+
+       
+    }
--- api/test.php
+++ api/test.php
@@ -5,8 +5,8 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 
-require_once dirname(__FILE__)."/services/UsersService.Class.php";
-$us = new UsersService();
-$result = $us->loadActiveUsers();
+require_once dirname(__FILE__)."/services/MessagesService.Class.php";
+$us = new MessagesService();
+$result = $us->loadMessages();
 print_r($result);
 
